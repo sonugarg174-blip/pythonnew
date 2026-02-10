fa268386d810d70b8cec594718d37787c961559d
sonugarg174-blip <sonugarg174@gmail.com>
2026-02-10 12:36:29 +0530

    Update activity1.py

--- lesson 2/activity1.py
+++ lesson 2/activity1.py
@@ -1,5 +1,5 @@
-name = "Soha"
-year = 8
+name = "Sonal"
+year = 34
 print(f"my name is {name} and i am in year {year}") 
 print ("Hello", end=" ")
-print("my name is Soha")
+print("my name is Sonal")
