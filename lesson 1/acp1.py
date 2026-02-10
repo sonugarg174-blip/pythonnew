86492181d1e194c6eb7b4f083a671610551b75aa
sonugarg174-blip <sonugarg174@gmail.com>
2026-02-10 12:22:35 +0530

    Add files via upload

---
+++ lesson 1/acp1.py
@@ -0,0 +1,3 @@
+print("The quotent of 6 divided by 4 in integers is", int(6/4))
+print("The remainder of the above is", 6%4)
+print("However the full answer of the above is", 6/4)
---
+++ lesson 1/main.py
@@ -0,0 +1,3 @@
+print("Hello")
+print(12+34)
+print("square of 5 is:", 5*5)
