9690b9bbbe18c9e51b449bdce50a1d3a4c675004
sonugarg174-blip <sonugarg174@gmail.com>
2026-06-29 13:08:51 +0530

    Update activity5.py

--- lesson 49/activity5.py
+++ lesson 49/activity5.py
@@ -1,6 +1,11 @@
-stars = "*"
-rows = int(input("Enter a number: "))
-count = 1
-for i in range(rows):
-    print(stars*count)
-    count += 1 
+num = int(input("Enter a number: "))
+
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
